4a1ad1c5ab3add8551773f5d5ea6b1fd43e5f8c9
parkermmr <parkerrennie@outlook.com>
2024-09-20 18:35:06 +1000

    wip(init): Quick commit of assignment 2 template files and task files.

---
+++ src/missionA2S1.m
@@ -0,0 +1,11 @@
+%% EGB242 Assignment 2, Section 1 %%
+% This file is a template for your MATLAB solution to Section 1.
+%
+% Before starting to write code, generate your data with the ??? as
+% described in the assignment task.
+
+%% Initialise workspace
+clear all; close all;
+load DataA2 audioMultiplexNoisy fs sid;
+
+% Begin writing your MATLAB solution below this line.
