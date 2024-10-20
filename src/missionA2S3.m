559d894ad49281fd911fc0edd05ff0640ef0551e
parkermmr <parkerrennie@outlook.com>
2024-10-20 13:49:18 +1000

    refact(): Preformed some minor code refactoring.

--- src/missionA2S3.m
+++ src/missionA2S3.m
@@ -26,8 +26,8 @@ imwrite(firstImage2D, 'FirstLandingSiteImage.png');
 %% 3.2
 
 % Image dimensions and parameters
-numPixels = 307200; % Number of pixels in an image (640x480)
-sampleRate = 1000;  % Pixels per second
+numPixels = 307200; 
+sampleRate = 1000;  
 
 % Time vector for the entire duration of one image
 t = linspace(0, numPixels / sampleRate, numPixels);
@@ -35,27 +35,22 @@ t = linspace(0, numPixels / sampleRate, numPixels);
 % Frequency vector calculation
 f = linspace(-sampleRate/2, sampleRate/2, numPixels);
 
-% Reshape the 1D image data to a 2D matrix
-imageData = imagesReceived(1, :); % Assuming the first row for the first image
-image2D = reshape(imageData, [480, 640]);
+% Reshaped the 1D image data to a 2D matrix
+imageData = imagesReceived(1, :);
+firstImage2D = reshape(firstImageData, [480, 640]);
 
-% Visualizing the image in the time domain
 figure;
-plot(t, imageData);
+plot(t, firstImageData);
 title('Time Domain Representation of Received Image Data');
 xlabel('Time (seconds)');
 ylabel('Pixel Intensity');
 
-% Visualizing the image in the frequency domain
 % Perform Fourier Transform
-imageDataFFT = fft(imageData);
-imageDataFFT = fftshift(imageDataFFT); % Centering zero frequency component
+imageDataFFT = fft(firstImageData);
+imageDataFFT = fftshift(imageDataFFT);
 
 figure;
 plot(f, abs(imageDataFFT));
 title('Frequency Domain Representation of Received Image Data');
 xlabel('Frequency (Hz)');
 ylabel('Magnitude');
-
-%% Save the time and frequency data
-save('time_and_frequency_vectors.mat', 't', 'f');
